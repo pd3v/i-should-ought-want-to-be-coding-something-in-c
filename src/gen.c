7c7c91694962e7da73d572d5e031b457fb5b7ab0
pd3v <paulo.develop@gmail.com>
2024-04-02 13:30:53 +0100

    refactoring 'rndW' function

--- src/gen.c
+++ src/gen.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "gen.h"
+
+struct midiPair;
 
 midi_t sum(midi_t value) {
 	temp += value;
@@ -15,6 +15,9 @@ midi_t rnd(midi_t min , midi_t max) {
 	return min + rand() % (max - min);
 }
 
-midi_t rndw(midi_t* arr, size_t size) {
-	return arr[0];
+void rndW(midi_t* arr, size_t size) {
+	for (int i = 0; i < size; i++) {
+		midiPairStack[i].value = arr[i];
+		midiPairStack[i].weight = rnd(1, 10);
+	}
 }
--- src/gen.h
+++ src/gen.h
@@ -1,9 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef unsigned int midi_t;
 
+struct midiPair {
+	midi_t value; 
+	unsigned int weight;
+};
+
 midi_t midiStack[100];
+struct midiPair midiPairStack[100];
 midi_t temp;
 
 midi_t sum(midi_t value);
 midi_t avg(midi_t sumTotal, unsigned int _counter);
 midi_t rnd(midi_t min , midi_t max);
-midi_t rndw(midi_t* arr, size_t size);
+void rndW(midi_t* arr, size_t size);

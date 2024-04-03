8586187e31c9c9c8026fb7cdc331c34289fdf911
pd3v <paulo.develop@gmail.com>
2024-04-03 17:38:37 +0100

    re-refactor 'rndW'

--- src/gen.c
+++ src/gen.c
@@ -7,17 +7,19 @@ midi_t sum(midi_t value) {
 	return temp;
 }
 
-midi_t avg(midi_t sumTotal, unsigned int _counter) {
-	return  sumTotal / _counter;
+midi_t avg(midi_t sumTotal, unsigned int counter) {
+	return  sumTotal / counter;
 }
 
 midi_t rnd(midi_t min , midi_t max) {
 	return min + rand() % (max - min);
 }
 
-void rndW(midi_t* arr, size_t size) {
-	for (int i = 0; i < size; i++) {
-		midiPairStack[i].value = arr[i];
-		midiPairStack[i].weight = rnd(1, 10);
-	}
+struct MidiPair* rndW(midi_t* arr, size_t size) {
+	struct MidiPair* _midiPairs = (struct MidiPair*)  malloc(sizeof (struct MidiPair) * size);
+	 
+	for (int i = 0; i < size; i++)
+		_midiPairs[i] = (struct MidiPair){.value = arr[i], .other = rnd(1, 10)};
+
+	return _midiPairs;
 }
--- src/gen.h
+++ src/gen.h
@@ -3,16 +3,16 @@
 
 typedef unsigned int midi_t;
 
-struct midiPair {
+struct MidiPair {
 	midi_t value; 
-	unsigned int weight;
+	unsigned int other;
 };
 
 midi_t midiStack[100];
-struct midiPair midiPairStack[100];
+struct MidiPair midiPairStack[100];
 midi_t temp;
 
 midi_t sum(midi_t value);
-midi_t avg(midi_t sumTotal, unsigned int _counter);
+midi_t avg(midi_t sumTotal, unsigned int counter);
 midi_t rnd(midi_t min , midi_t max);
-void rndW(midi_t* arr, size_t size);
+struct MidiPair* rndW(midi_t* arr, size_t _size);
